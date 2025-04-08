2bf9cc696ded231a1d0e9a4624e684ebebfe89cb
Evan <evan.southwick5@gmail.com>
2025-04-08 16:20:46 -0600

    Added settings code

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatLabel} from '@angular/material/input';
+import {MatSelect} from '@angular/material/select';
+import {MatOption} from '@angular/material/core';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatOption
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  displayMode = 'normal';
 
+  updateDisplayMode(mode: string) {
+    document.querySelector('mat-sidenav-container')?.classList.remove('normal', 'dark', 'high-contrast');
+    document.querySelector('mat-sidenav-container')?.classList.add(mode);
+  }
 }
