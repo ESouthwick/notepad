63c2a4a5959ac40a6544a800e9ca9567a290750c
Evan <evan.southwick5@gmail.com>
2025-04-08 14:51:33 -0600

    init commit

---
+++ src/app/components/notes-organize/notes-organize.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-organize',
+  imports: [],
+  templateUrl: './notes-organize.component.html',
+  styleUrl: './notes-organize.component.css'
+})
+export class NotesOrganizeComponent {
+
+}
---
+++ src/app/services/note.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {Note} from '../model/note.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class NoteService {
+}
